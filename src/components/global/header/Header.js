90afeae38b313831ecb48c01931679796b9b853f
RamziBach <ramzibach.dev@gmail.com>
2021-02-16 13:41:33 -0500

    tooltip pop

--- src/components/global/header/Header.js
+++ src/components/global/header/Header.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import styles from './header.module.css';
 
 const Header = ({ price }) => {
+  const [opacity, setOpacity] = useState(0);
+
+  const onMouseEnter = () => setOpacity(1);
+
+  const onMouseLeave = () => setOpacity(0);
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -49,9 +56,34 @@ const Header = ({ price }) => {
             >
               ${price}
             </a>
-            <button title="What is this number?">
+            <button onMouseEnter={onMouseEnter} title="Tooltip">
               <i className={`fas fa-info-circle ${styles.i}`}></i>
             </button>
+            <div
+              style={{ opacity: opacity }}
+              className={styles.tooltipContainer}
+            >
+              <div className={styles.arrowUp}></div>
+              <div className={styles.tooltipHeader}>
+                <div className={styles.tooltipHeaderChild1}>
+                  <i className={`fas fa-info-circle ${styles.ii}`}></i>
+                  <h5 className={styles.tooltipTitle}>what is this number?</h5>
+                </div>
+                <button className={styles.close} onClick={onMouseLeave}>
+                  <i className={`fas fa-times ${styles.ii}`}></i>
+                </button>
+              </div>
+              <p className={styles.tooltipDescription}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+              <div>
+                <button id={styles.btn1} className={styles.tooltipBtn}>
+                  buy now
+                </button>
+                <button className={styles.tooltipBtn}>learn more</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
