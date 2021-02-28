4b82da8f8900b2d1a20f0a4c5d2b4272c19a3470
RamziBach <ramzibach.dev@gmail.com>
2021-02-28 18:01:33 -0500

    next-auth

--- src/pages/vip.js
+++ src/pages/vip.js
@@ -1,14 +1,26 @@
+import { signIn, signOut, useSession } from 'next-auth/client';
 import Seo from '../components/global/seo/Seo';
 import Header from '../components/global/header/Header';
 import Landing from '../components/vip/landing/Landing';
 import Footer from '../components/global/footer/Footer';
 
 const Vip = ({ price }) => {
+  const [session, loading] = useSession();
+
   return (
     <>
       <Seo title="DRAZE - VIP" />
       <Header price={price} isOnVipPage={true} />
-      <Landing />
+      {!session && (
+        <>
+          <h3>Not signed in.</h3>
+          <button style={{ color: 'white' }} onClick={() => signIn()}>
+            Sign in
+          </button>
+        </>
+      )}
+      {session && <Landing />}
+      {/* <Landing /> */}
       <Footer />
     </>
   );
