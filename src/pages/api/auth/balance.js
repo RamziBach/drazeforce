5d8509050eea4636372a997eb47f883f6f9b8b43
RamziBach <ramzibach.dev@gmail.com>
2021-06-22 15:02:26 -0400

    balance

---
+++ src/pages/api/auth/balance.js
@@ -0,0 +1,25 @@
+import Cookies from 'cookies';
+import { access_token, httpGet } from '../../../../utils/helperFunctions';
+
+const getUserBalance = async (userId, res) => {
+  if (!access_token) {
+    res.status(401).json({ error: 'Application not registered with Rally' });
+  }
+  try {
+    const rally_response = await httpGet(
+      `${rally_api_url}/users/rally/${userId}/balance`,
+      { Authorization: 'Bearer ' + access_token }
+    );
+    const data = rally_response.data;
+    return data;
+  } catch (err) {
+    res.status(500);
+  }
+};
+
+export default async (req, res) => {
+  const cookies = new Cookies(req, res);
+  const userId = cookies.get('rnbUserId');
+  const balanceData = await getUserBalance(userId, res);
+  res.status(200).json(balanceData);
+};
