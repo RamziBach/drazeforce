d5ef66f8cb6734c03c93b598c99f6b01cd051703
RamziBach <ramzibach.dev@gmail.com>
2021-06-23 07:11:47 -0400

    gate

--- src/components/vip/landing/Index.js
+++ src/components/vip/landing/Index.js
@@ -1,6 +1,32 @@
 import styles from './index.module.css';
 
 const Index = () => {
+  const getBalance = async () => {
+    try {
+      const res = await fetch('/api/auth/balance');
+      const data = await res.json();
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const balance = getBalance();
+
+  if (balance.error)
+    return <div>You must be logged in to view this content.</div>;
+
+  const hasDraze = balance.some(item => item.coinKind === 'DRAZE');
+
+  if (!hasDraze) return <div>You do not own any $DRAZE coin.</div>;
+
+  const drazeBalance = balance.find(item => item.coinKind === 'DRAZE');
+
+  if (hasDraze && drazeBalance.coinBalance < 10)
+    return (
+      <div>You must own 10 or more $DRAZE coins to view this content.</div>
+    );
+
   return (
     <div data-scroll-section>
       <section className={styles.landing}>
